77173089366014f272f3eccb4650ac67d7a0d354
Felipe Lacerda Schoen <47650824+FelipeSchoen@users.noreply.github.com>
2025-01-02 00:31:00 -0300

    Add files via upload

---
+++ DIO NTT DATA - Engenharia de Dados com Python/Variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "Felipe"
+idade = 35
+
+nome, idade = "Gabriela", 34
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS", "MG"]
+
+print(BRAZILIAN_STATES)
+
