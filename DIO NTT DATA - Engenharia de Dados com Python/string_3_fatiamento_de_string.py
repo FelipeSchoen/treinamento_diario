6768f9898d5a0ae04cb598d57b46ec6504e30111
Felipe Lacerda Schoen <47650824+FelipeSchoen@users.noreply.github.com>
2025-01-10 01:04:35 -0300

    Add files via upload

---
+++ DIO NTT DATA - Engenharia de Dados com Python/string_3_fatiamento_de_string.py
@@ -0,0 +1,11 @@
+# FATIAMENTO
+nome = "Felipe Lacerda Schoen"
+
+print(nome[0])
+print(nome[:6])
+print(nome[7:])
+print(nome[7:14])
+print(nome[15:21])
+print(nome[7:14:2])
+print(nome[:])
+print(nome[::-1])
