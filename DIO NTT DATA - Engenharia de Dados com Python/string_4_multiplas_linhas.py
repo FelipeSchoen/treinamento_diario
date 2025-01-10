886761cc3842d262da489e89f42e596f1cfcaddd
Felipe Lacerda Schoen <47650824+FelipeSchoen@users.noreply.github.com>
2025-01-10 01:11:06 -0300

    Add files via upload

---
+++ DIO NTT DATA - Engenharia de Dados com Python/string_4_multiplas_linhas.py
@@ -0,0 +1,27 @@
+#string multi linha
+
+nome = "Felipe"
+
+mensagem = f"""
+      Olá! Meu nome é {nome},
+      Eu estou aprendendo Python.
+      Essa mensagem tem diferentes Recursos
+      """
+
+print(mensagem)
+
+
+menu = f"""
+
+        ===== MENU ======
+
+        1 - CONSULTAR SALDO
+        2 - SACAR
+        3 - DEPOSITAR
+        4 - SAIR
+
+        =================
+    """
+
+print(menu)
+
